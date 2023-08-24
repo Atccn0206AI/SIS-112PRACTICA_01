ba0366b4e383b53fdef6b798f705b3b19a6b93f9
Manuel Franco Jiménez Mendoza <142540969+Atccn0206AI@users.noreply.github.com>
2023-08-24 01:28:50 -0400

    Add files via upload

---
+++ Ejericicio_01_67.cpp
@@ -0,0 +1,42 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Willy Edwin Tenorio Palza
+// Fecha creación: 23/08/2023
+// Fecha modificación: 23/08/2023
+// Número de ejericio: 67
+// Problema planteado:   Leer un número entero y determinar si la suma de sus dígitos distintos de cero forman un número primo, de ser así imprima el mensaje respectivo.
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int numero;
+    cout << "Ingresa un numero entero: "; cin >> numero;
+    int sumaDigitos = 0;
+    int num = numero;
+    while (num > 0) {
+        int digito = num % 10;
+        if (digito != 0) {
+            sumaDigitos += digito;
+        }
+        num /= 10;
+    }
+    bool esPrimo = true;
+    if (sumaDigitos <= 1) {
+        esPrimo = false;
+    } else {
+        for (int i = 2; i <= sqrt(sumaDigitos); ++i) {
+            if (sumaDigitos % i == 0) {
+                esPrimo = false;
+                break;
+            }
+        }
+    }
+
+    if (esPrimo) {
+        cout << "La suma de los digitos distintos de cero forma un numero primo." << endl;
+    } else {
+        cout << "La suma de los digitos distintos de cero no forma un numero primo." << endl;
+    }
+
+    return 0;
+}
+
