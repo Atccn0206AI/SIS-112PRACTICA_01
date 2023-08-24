047d7251897dae6f098923ddc8f6e0a68ba536f1
Manuel Franco Jiménez Mendoza <142540969+Atccn0206AI@users.noreply.github.com>
2023-08-24 01:15:00 -0400

    Add files via upload

---
+++ Ejericicio_01_52.cpp
@@ -0,0 +1,13 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Willy Edwin Tenorio Palza
+// Fecha creación: 23/08/2023
+// Fecha modificación: 23/08/2023
+// Número de ejericio: 52
+// Problema planteado:  Obtener xy utilizando productos sucesivos, tanto x como y son enteros positivos o negativos.
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Parece mal planteado";
+	return 0;
+}
